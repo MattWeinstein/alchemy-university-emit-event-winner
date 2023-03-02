eefa433b56d92fca252f83d04e0f038cf5504d63
MattWeinstein <102390205+MattWeinstein@users.noreply.github.com>
2023-03-02 01:08:02 -0500

    updates readme, removed unused comments

--- scripts/attemptCall.js
+++ scripts/attemptCall.js
@@ -18,15 +18,3 @@ main().catch((error) => {
 
 
 // Succesful txn - https://goerli.etherscan.io/tx/0x56d27eb8822f6b144b525ef44d3512e1290695e36462eb46abfad136564a7b80
-
-
-// The goal of this project was to call a function on a solidity contract, not directly from the contract
-// The task was tx.origin cannot equal msg.sender
-    // msg.sender is what initiated the request. This will be the function in the contract, and msg.sender = contract address
-    // tx.origin, is that initiated the entire txn. This is the EOA you connected to using your private key in the deploy.js
-
-// Why does this matter?
-    // Someone (EOA) can connect to a smart contract and make purchases or other stuff
-    // You can make calls from one smart contract to another, and track the account which made it.
-    // You can do anything with this info
-        // Ex. allow/disallow access, give NFT, keep track of who interacted
